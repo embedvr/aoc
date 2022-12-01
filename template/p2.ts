dc09ac6cdd886a44e3a310fa3ccf2d0b38360aed
Brydon L <brydon@astolfo.co>
2022-12-01 11:24:41 -0500

    [~] fix template

--- template/p2.ts
+++ template/p2.ts
@@ -1,13 +1,1 @@
 const input = await Deno.readTextFile('in.txt');
-const chunks = input.split('\n\n');
-let largestNumbers: number[] = [];
-chunks.forEach(elf => {
-    let lines = elf.split('\n');
-    let sum = 0;
-    lines.forEach(line => {
-        sum += parseInt(line);
-    });
-    largestNumbers.push(sum);
-});
-let top3 = largestNumbers.sort((a, b) => b - a).slice(0, 3);
-console.log(top3[0] + top3[1] + top3[2]);
